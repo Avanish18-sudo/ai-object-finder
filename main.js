dc039804269b508e248b07f16adbc13f92d6be15
Avanish18-sudo <85296627+Avanish18-sudo@users.noreply.github.com>
2021-06-29 19:52:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+status="";
+
+function start(){
+    objectDetector = ml5. objectDetector( 'cocossd', modelLoaded) ;
+     document. getElementById("status") . innerHTML = "Status : Detecting Objects";
+     object_name=document.getElementById("input").value;
+}
+function modelLoaded(){
+    console.log("modelLoaded");
+    status=true;
+}
+function preload(){
+
+}
+function draw(){
+image(video,0,0,380,380)
+}
+function setup(){
+canvas=createCanvas(380,380);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+}
